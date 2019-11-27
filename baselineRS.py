3192fa301dfaf785b50fe9a07539ff806270388c
keyber <keyvan.beroukhim@etu.upmc.fr>
2019-11-27 22:17:28 +0100

    ajout commentaires

--- baselineRS.py
+++ baselineRS.py
@@ -1,9 +1,9 @@
-import random
-
+from architecture import Architecture
 
 class AgentRS:
     def __init__(self, space):
+        """Retourne une architecture aléatoire à chaque fois"""
         self.space = space
     
     def act(self, last_reward=None):
-        return random.choice(self.space)
+        return Architecture.random_sample(self.space)
