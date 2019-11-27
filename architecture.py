772c01c09e1ac2be065d03d2dd2d2d3447ba4511
keyber <keyvan.beroukhim@etu.upmc.fr>
2019-11-27 22:18:32 +0100

    code fonctionnel

--- architecture.py
+++ architecture.py
@@ -1,60 +1,47 @@
 import torch.nn as nn
 from torch.optim import Adam
-from random import random, sample
-import numpy as np
-
-SEARCH_SPACE = {
-    # "input" : (0, 1, 2),
-    "fine_tune_input" : (True, False),
-    "use_conv" : (True, False),
-    "conv_act" : (nn.ReLU(), nn.LeakyReLU(), nn.Sigmoid(), nn.Tanh()),
-    "conv_batch_norm": (True, False),
-    "conv_max_ngram_length": (2, 3),
-    "conv_dropout": (.0, .1, .2, .3, .4),
-    "conv_filter_nb": (32, 64, 128),
-    "hid_layer_nb": (0, 1, 2, 3, 5),
-    "hid_layer_size" : (64, 128, 256),
-    "hid_layer_act" : (nn.ReLU(), nn.LeakyReLU(), nn.Sigmoid(), nn.Tanh()),
-    "hid_layer_norma" : (None, "batch", "layer"),
-    "hid_layer_dropout" : (.0, .1, .2, .3, .4),
-    # "optim" : (),
-    "batch_size" : (128, 256),
-    "deep_tower_lr" : np.logspace(1e-3, 1e-1, 5),
-    "deep_tower_regularization_weight" : (0., 1e-4, 1e-3, 1e-2),
-    "wide_tower_lr" : np.logspace(1e-3, 1e-1, 5),
-    "wide_tower_regularization_weight" : (0., 1e-4, 1e-3, 1e-2),
-    "nb_training_samples" : (1e5, 1e6),
-}
+from random import random, choice
 
 
 class Architecture:
-    def __init__(self, search_space:dict, archi:dict):
+    """Contient un réseau de neurones, une loss et un optimiser"""
+    
+    def __init__(self, search_space, architecture):
         self.search_space = search_space
-        self.hyperparam_value = archi
-        self.net = self._create_model(archi)
-        self.loss = nn.BCEWithLogitsLoss()
+        self.architecture = architecture
+        
+        self.net = search_space.create_model(architecture)
+        self.loss = nn.CrossEntropyLoss()
         self.optim = Adam(self.net.parameters())
     
-    @staticmethod
-    def _create_model(archi):
-        # text embedding module
-        
-        # fc stack
-        
-        
-        return nn.Sequential(*[])
-    
-    #wide shallow layer OneHot->softmax, L1 loss
-    
     def mutate(self, r=.5):
-        archi = {}
-        for k in self.search_space:
-            archi[k] = sample(self.search_space[k]) if random() < r else self.hyperparam_value[k]
+        """Retourne une nouvelle architecture en rechoisissant les caractéristiques avec une proba r"""
+        space = self.search_space.space
         
+        global_values = {k: choice(space["global"][k]) for k in space["global"]}
+        
+        n = global_values["layer_number"]
+        local_values = []
+        for i in range(n):
+            if i < self.architecture["global"]["layer_number"]:
+                local_values.append({k: choice(space["local"][k]) if random() < r 
+                                     else self.architecture["local"][i][k]
+                                     for k in space["local"]})
+            else:
+                local_values.append({k: choice(space["local"][k]) for k in space["local"]})
+        
+        archi = {"global": global_values, "local": local_values}
         return Architecture(self.search_space, archi)
-
+    
     @staticmethod
     def random_sample(search_space):
-        archi = {k: sample(search_space[k]) for k in search_space}
+        """Retourne une architecture aléatoire de l'espace"""
+        space = search_space.space
+        
+        global_values = {k: choice(space["global"][k]) for k in space["global"]}
+        
+        n = global_values["layer_number"]
+        local_values = [{k: choice(space["local"][k]) for k in space["local"]} for _ in range(n)]
+        
+        archi = {"global": global_values, "local": local_values}
         return Architecture(search_space, archi)
-
