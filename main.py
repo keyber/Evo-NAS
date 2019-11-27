3d1046366220189bb637cb079c5dd5a5376e3437
keyber <keyvan.beroukhim@etu.upmc.fr>
2019-11-27 22:23:36 +0100

    compare RS et Evo

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import torch
 import torch.utils.data
-
+import datasets
 from baselineRS import AgentRS
 from baselineEvo import AgentEvo
 import numpy as np
 
-def fit_model(model, data, n_epochs=10):
+def fit_model(model, data, n_epochs=2, max_iter=1e8):
+    i = 0
     for epoch in range(n_epochs):
         for x, y in data:
-            model.net.zero_grads()
+            model.net.zero_grad()
             y_pred = model.net(x)
             loss = model.loss(y_pred, y)
             loss.backward()
             model.optim.step()
+            i += 1
+            
+            if i==max_iter:
+                return 
         
         model.lr_scheduler.step(epoch)
         
@@ -29,39 +34,42 @@ def score_model(model, data):
     return loss
 
 
-def run_agent(ds_train, ds_test, agent, n_iter=10, batch_size=32, verbose=0):
+def run_agent(ds_train, ds_test, agent, n_iter=10, train_max_iter=1e8, batch_size=7, verbose=0):
     train = torch.utils.data.DataLoader(ds_train, batch_size=batch_size, drop_last=True)
     test = torch.utils.data.DataLoader(ds_test, batch_size=batch_size, drop_last=True)
     best_a, best_v = None, -np.inf
     
-    archi = agent.act(reward=None)
+    archi = agent.act(last_reward=None)
     
     
     for i in range(n_iter):
-        model = archi.model()
-        fit_model(model, train)
-        score = score_model(model, test)
+        fit_model(archi, train, max_iter=train_max_iter)
+        score = score_model(archi, test)
         
         if score > best_v:
             best_v = score
             best_a = archi
             if verbose==1:
-                print("archi", archi, "score", score)
+                print(score, archi.architecture)
                 
         if verbose==2:
-            print("archi", archi, "score", score)
+            print(score, archi.architecture)
         
-        archi = agent.act(reward=score)
+        archi = agent.act(last_reward=score)
         
     
     return best_a, best_v
         
 
 def main():
-    space = []
-    agents = [AgentRS(space), AgentEvo(space, P=10, S=5)]
+    space, ds_train, ds_test = datasets.cifar10(reduced=True)
+    agents = [AgentEvo(space, min_p=3, max_p=5, s=2), AgentRS(space)]
     
     for agent in agents:
-        print("Agent", agent)
-        best_arch, best_val = run_agent(ds_train, ds_test, agent)
-        print("best_arch", best_arch, "best_val", best_val)
+        print("Agent", agent.__class__)
+        best_arch, best_val = run_agent(ds_train, ds_test, agent, n_iter=10, train_max_iter=2, verbose=2)
+        print("best archi", best_arch.architecture)
+        print("best val", best_val)
+        print("\n\n\n")
+
+main()
