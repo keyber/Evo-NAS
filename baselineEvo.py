40e9f81cf03103b58fc09b1fdbe259ea2e95e918
keyber <keyvan.beroukhim@etu.upmc.fr>
2019-11-27 22:17:54 +0100

    code fonctionnel

--- baselineEvo.py
+++ baselineEvo.py
@@ -1,12 +1,21 @@
-import numpy as np
 from collections import deque
+from architecture import Architecture
+from random import sample
 
 
 class AgentEvo:
-    def __init__(self, space, P, S):
+    def __init__(self, space, min_p, max_p, s, mutation_proba=.5):
+        """
+        :param min_p: les min_p premiers indivisus sont tirés aléatoirement
+        :param max_p: au plus max_p individus sont gardés
+        :param s: on fait muter le meilleur parmi s individus
+        """
         self.space = space
-        self.P = P
-        self.S = S
+        self.s = s
+        self.max_p = max_p
+        self.min_p = min_p
+        self.mutation_proba = mutation_proba
+        
         self.population = deque()
         self.last_action = None
         
@@ -15,18 +24,28 @@ class AgentEvo:
         # prise en compte du feedback
         if self.last_action is not None:
             self.population.append((self.last_action, last_reward))
+        
+        
+        if len(self.population) < self.min_p:
+            # les min_p premiers individus sont tirés aléatoirement
+            selection = Architecture.random_sample(self.space)
             
-        # enlève la plus ancienne des architectures
-        if len(self.population) == self.P + 1:
+        else:
+            # choisit S architectures aléatoirement
+            selection = sample(self.population, self.s)
+            
+            # en extrait la meilleure
+            selection = max(selection, key=lambda x:x[1])[0]
+            
+            # lui fait subir une mutation
+            selection = selection.mutate(r=self.mutation_proba)
+        
+        
+        if len(self.population) > self.max_p:
+            # enlève la plus ancienne des architectures
             self.population.popleft()
         
-        # choisit S architectures aléatoirement
-        selection = np.random.choice(self.population, self.S)
         
-        # en extrait la meilleure
-        selection = max(selection, key=lambda x:x[1])
-        
-        # lui fait subir une mutation
-        selection = selection.mutate()
+        self.last_action = selection
         
         return selection
